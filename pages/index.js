9678a98aaf8ad801db71f08245cf0b264cce08ce
Diogo Barreto <66051980+Dihh-barret@users.noreply.github.com>
2024-09-12 17:26:06 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
 function home() {
-    return <h2>Mais uma mudança no textinho desse projeto.</h2> 
+  return <h2>Mais uma mudança no textinho desse projeto.</h2>;
+}
+
+function test() {
+  console.log("teste");
+}
+
+function testIdentation() {
+  console.log("Teste de identação");
+  console.log("Teste de identação 2");
 }
 export default home;
